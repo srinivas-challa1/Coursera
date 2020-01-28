7f20e431dd9b5cb003b534597ef4ce8fc5a0d8a2
srinivas-challa1 <srinivas.srinivas.challa@gmail.com>
2020-01-28 16:04:41 +0530

    commit

--- week1/assignment1.1.py
+++ week1/assignment1.1.py
@@ -15,3 +15,57 @@ with open("emotion_words.txt","r") as emotions:
         k=len(line.strip().split())
         num_words+=k
     print(num_words)
+
+'''
+Assign to the variable num_lines the number of lines in the file school_prompt.txt.
+'''
+with open("school_prompt.txt","r") as school:
+    lines=school.readlines()
+    num_lines=len(lines)
+    print(num_lines)
+
+'''
+Assign the first 30 characters of school_prompt.txt as a string to the variable beginning_chars.
+'''
+
+with open("school_prompt.txt","r") as school:
+    school_data=school.read()
+    beginning_chars=school_data[:30]
+    print(beginning_chars)
+
+'''
+Challenge: Using the file school_prompt.txt, assign the third word of every line to a list called three.
+'''
+with open("school_prompt.txt","r") as school:
+    lines=school.readlines()
+    three=[]
+    for line in lines:
+        line=line.strip().split()
+        three.append(line[2])
+print(three)
+
+#Challenge: Create a list called emotions that contains the first word of every line in emotion_words.txt.
+with open("emotion_words.txt","r") as emotional:
+    lines=emotional.readlines()
+    emotions=[]
+    for line in lines:
+        line=line.strip().split()
+        emotions.append(line[0])
+print(emotions)
+
+
+#Assign the first 33 characters from the textfile, travel_plans.txt to the variable first_chars
+with open("travel_plans.txt") as travel_plans:
+    string=travel_plans.read()
+    first_chars=string[:33]
+    print(first_chars)
+#Challenge: Using the file school_prompt.txt, if the character ‘p’ is in a word, then add the word to a list called p_words.
+with open("school_prompt.txt","r") as school:
+    lines=school.readlines()
+    p_words=[]
+    for line in lines:
+        line=line.strip().split()
+        for word in line:
+            if "p" in word:
+                p_words.append(word)
+print(p_words)
